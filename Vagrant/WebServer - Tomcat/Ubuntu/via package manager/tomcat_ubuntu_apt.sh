8776c695f06f67566c236d7692420c63152242c3
SirineHassine <80529376+SirineHassine@users.noreply.github.com>
2024-11-28 18:47:49 +0100

    Develop (#3)
    * Feature/vagrant (#1)
    
    * add Vagrant scripts, for Tomcat, Bind, MariaDB and multiVM
    
    * update readme
    
    * delete .vagrant folders generated
    
    * Merge feature/AWS to Develop
    
    * Add Docker files
    
    * Merge Kubernetes branch into develop
    
    * Merge Helm branch to develop
    
    * Add Terraform files
    
    * Update Terraform readme
    
    * Merge Jenkins to develop
    
    * update readme docker
    
    * add myresumefiles
    
    * add myresumefiles

--- Vagrant/WebServer - Tomcat/Ubuntu/via package manager/tomcat_ubuntu_apt.sh
+++ Vagrant/WebServer - Tomcat/Ubuntu/via package manager/tomcat_ubuntu_apt.sh
@@ -6,15 +6,17 @@ sudo apt upgrade -y
 
 # Install Java ands Tomcat
 sudo apt install openjdk-11-jdk -y
-sudo apt install tomcat9 tomcat9-admin tomcat9-docs tomcat9-common git -y
+
+sudo apt install tomcat10 tomcat10-admin tomcat10-docs tomcat10-common git -y
 
 
 # start and enable Tomcat at restart
-sudo systemctl start tomcat9
-sudo systemctl enable tomcat9
+sudo systemctl start tomcat10
+sudo systemctl enable tomcat10
 
 # Créer un fichier index.html pour tester
-echo "<html><body><h1>Tomcat fonctionne via apt sur Ubuntu APT !</h1></body></html>" | sudo tee /var/lib/tomcat9/webapps/ROOT/index.html
+echo "<html><body><h1>Tomcat fonctionne via apt sur Ubuntu APT !</h1></body></html>" | sudo tee /var/lib/tomcat10/webapps/ROOT/index.html
 
 # Verify  Tomcat is active
-sudo systemctl status tomcat9
+sudo systemctl status tomcat10
+
